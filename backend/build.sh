5bb3c575a9be9f47322519c14d4aeec80eca1abc
Gustavo LP <gust.20220@gmail.com>
2025-10-07 16:56:51 -0400

    render fix

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Crear directorio para archivos estáticos
+mkdir -p staticfiles
+
 # Collect static files
 python manage.py collectstatic --no-input
 
