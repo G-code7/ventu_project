8ca129a6db1a1e3784916263aa101cf87497c552
Gustavo LP <gust.20220@gmail.com>
2025-10-18 18:58:47 -0400

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,26 +2,34 @@ import { defineConfig, loadEnv } from 'vite'
 import dns from 'node:dns'
 import react from '@vitejs/plugin-react'
 
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig(({ mode }) => {
-  // Cargar variables de entorno según el modo
   const env = loadEnv(mode, process.cwd(), '')
+  const apiTarget = env.VITE_API_URL || 'http://localhost:8000';
   
-  return {
+return {
     plugins: [react()],
+    
     server: {
+      allowedHosts: [
+        'ventu-website.onrender.com', 
+        'localhost',
+      ],
+      host: '0.0.0.0', 
       proxy: {
         '/api': {
-          target: env.VITE_API_URL || 'http://localhost:8000',
+          target: apiTarget,
           changeOrigin: true,
           secure: false,
         }
       },
-      open: 'ventu-website.onrender.com',
     },
+    
     build: {
       outDir: 'dist',
       sourcemap: false,
-    // Configuración base para deployment
-        base: '/',
+      base: '/', 
+    }
   }
-}})
+})
